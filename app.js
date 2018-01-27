621f435dcc9a7a7dfb5d83a15596a1efae9ab3b1
Oleg Smirnov <helg@shaman-art.com>
2018-01-27 19:28:48 +0300

    Добавлен файл стилей, отрисованы первые фигуры

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 "use strict";
 
+let canvas = document.querySelector('.tutorial'),
+	ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgb(200,0,0)';
+ctx.fillRect(10,10,55,50);
+
+ctx.fillStyle = 'rgba(0,0,200,.5)';
+ctx.fillRect(30,30,55,50);
